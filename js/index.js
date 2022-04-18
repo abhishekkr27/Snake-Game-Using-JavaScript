830adbc4dd9b1ef95c44a9505bcfb5a1af40635e
Abhishek Kumar <abhishekrauniyar2@gmail.com>
2022-04-18 15:43:37 +0530

    Add Music and Some Changes

--- js/index.js
+++ js/index.js
@@ -1,5 +1,10 @@
 //     <!-- GAME JAVASCRIPT -->
 
+const foodSound = new Audio("music/food.mp3");
+const gameOverSound = new Audio("music/gameover.mp3");
+const moveSound = new Audio("music/move.mp3");
+const musicSound = new Audio("music/music.mp3");
+
 // Environment variables.
 // This code is only executed once.
 var GRID_HEIGHT = 40; // = 600 / 15
@@ -7,11 +12,11 @@ var GRID_WIDTH = 60; // = 900 / 15
 var BLOCK_SIZE = 15;
 // Accesses the correct COL_CHANGE and ROW_CHANGE
 var DIRECTIONS = {
-    up: 0,
-    right: 1,
-    down: 2,
-    left: 3
-}
+  up: 0,
+  right: 1,
+  down: 2,
+  left: 3,
+};
 /*
         0
     3       1
@@ -19,11 +24,11 @@ var DIRECTIONS = {
 */
 var COL_CHANGE = [0, 1, 0, -1];
 var ROW_CHANGE = [-1, 0, 1, 0];
-                //slow, med, fast
+//slow, med, fast
 var GAME_SPEEDS = [
-    {name: "Slow", speed: 200},
-    {name: "Medium", speed: 140},
-    {name: "Fast", speed: 80}
+  { name: "Slow", speed: 200 },
+  { name: "Medium", speed: 140 },
+  { name: "Fast", speed: 80 },
 ];
 var SNAKE_DIRECTION = DIRECTIONS["up"];
 var IS_DYING = false;
@@ -46,22 +51,24 @@ var gameSpeedButton = document.getElementById("game-speed");
 
 // Handle user input
 document.onkeydown = checkKey;
-function checkKey(e) {
-    e = e || window.event;
 
-    if (e.keyCode == '38') {
-        // up arrow
-        SNAKE_DIRECTION = DIRECTIONS["up"];
-    } else if (e.keyCode == '40') {
-        // down arrow
-        SNAKE_DIRECTION = DIRECTIONS["down"];
-    } else if (e.keyCode == '37') {
-        // left arrow
-        SNAKE_DIRECTION = DIRECTIONS["left"];
-    } else if (e.keyCode == '39') {
-        // right arrow
-        SNAKE_DIRECTION = DIRECTIONS["right"];
-    }
+function checkKey(e) {
+  moveSound.play();
+  e = e || window.event;
+
+  if (e.keyCode == "38") {
+    // up arrow
+    SNAKE_DIRECTION = DIRECTIONS["up"];
+  } else if (e.keyCode == "40") {
+    // down arrow
+    SNAKE_DIRECTION = DIRECTIONS["down"];
+  } else if (e.keyCode == "37") {
+    // left arrow
+    SNAKE_DIRECTION = DIRECTIONS["left"];
+  } else if (e.keyCode == "39") {
+    // right arrow
+    SNAKE_DIRECTION = DIRECTIONS["right"];
+  }
 }
 
 var snake = [];
@@ -82,49 +89,57 @@ snake.push(snakeHead);
 
 // Will execute function() every CURRENT_GAME_SPEED.speed milliseconds
 function play() {
-    GAME_LOOP = setInterval(function () {
+  GAME_LOOP = setInterval(function () {
 
-        ///////////////////////// Main Game Loop //////////////////////
+    ///////////////////////// Main Game Loop //////////////////////
 
-        // Move the snake one space in it's current direction.
-        snake.unshift(new SnakePiece(snake[0].row + ROW_CHANGE[SNAKE_DIRECTION], snake[0].col + COL_CHANGE[SNAKE_DIRECTION]));
+    // Move the snake one space in it's current direction.
+    snake.unshift(
+      new SnakePiece(
+        snake[0].row + ROW_CHANGE[SNAKE_DIRECTION],
+        snake[0].col + COL_CHANGE[SNAKE_DIRECTION]
+      )
+    );
 
-        IS_EATING = isEating();
-        if (IS_EATING) {
-            gameScore = gameScore + 1;
-            console.log("YOU ATE A FOOD");
-        }
-        // move tail of snake
-        if (!IS_EATING) {
-            gameGrid.removeChild(snake.pop().el);
-        }
+    IS_EATING = isEating();
+    if (IS_EATING) {
+      foodSound.play();
+      gameScore = gameScore + 1;
+      console.log("YOU ATE A FOOD");
+    }
+    // move tail of snake
+    if (!IS_EATING) {
+      gameGrid.removeChild(snake.pop().el);
+    }
 
-        IS_DYING = isDying();
-        if (IS_DYING) {
-            // The snake collided with a wall or with itself.
-            PLAY_GAME = false;
-            handleDeath();
-            console.log("YOU'VE DIED");
-        }
+    IS_DYING = isDying();
+    if (IS_DYING) {
+      musicSound.pause()
+      gameOverSound.play();
+      // The snake collided with a wall or with itself.
+      PLAY_GAME = false;
+      handleDeath();
+      console.log("YOU'VE DIED");
+    }
 
-        // End of cycle logic.
-        IS_EATING = false;
-        IS_DYING = false;
+    // End of cycle logic.
+    IS_EATING = false;
+    IS_DYING = false;
 
-        console.log(SNAKE_DIRECTION);
-        // console.log(foods);
-        scoreText.innerText = "Your Score: " + gameScore;
-        if(!PAUSE_FOR_DEATH) {
-            playButton.innerText = PLAY_GAME ? "End Game" : "Play Game";
-        }
+    console.log(SNAKE_DIRECTION);
+    // console.log(foods);
+    scoreText.innerText = "Your Score: " + gameScore;
+    if (!PAUSE_FOR_DEATH) {
+      playButton.innerText = PLAY_GAME ? "End Game" : "Play Game";
+    }
 
-        // Spawn more foods if they're all gone.
-        if (foods.length < 1) {
-            foods.push(new FoodPiece());
-            foods.push(new FoodPiece());
-            foods.push(new FoodPiece());
-        }
-    }, CURRENT_GAME_SPEED.speed);
+    // Spawn more foods if they're all gone.
+    if (foods.length < 1) {
+      foods.push(new FoodPiece());
+      foods.push(new FoodPiece());
+      foods.push(new FoodPiece());
+    }
+  }, CURRENT_GAME_SPEED.speed);
 }
 
 /*
@@ -132,59 +147,61 @@ function play() {
  */
 
 function handleToggleGamePlay() {
-    PLAY_GAME = !PLAY_GAME;
-    console.log("PLAY_GAME ?: ",PLAY_GAME);
-    if(PLAY_GAME) {
-        play();
-    } else {
-        clearInterval(GAME_LOOP);
-    }
+  PLAY_GAME = !PLAY_GAME;
+  console.log("PLAY_GAME ?: ", PLAY_GAME);
+  if (PLAY_GAME) {
+    musicSound.play();
+    play();
+  } else {
+    musicSound.pause();
+    clearInterval(GAME_LOOP);
+  }
 }
 
 function handleChangeGameSpeed() {
-    if (!PLAY_GAME) {
-        // We can only change the game speed while a game is not going
-        var currentSpeedIndex = GAME_SPEEDS.indexOf(CURRENT_GAME_SPEED);
-        var nextSpeedIndex = (currentSpeedIndex < GAME_SPEEDS.length - 1) ? (currentSpeedIndex + 1) : 0;
-        CURRENT_GAME_SPEED = GAME_SPEEDS[nextSpeedIndex];
-        console.log("updated game speed:",CURRENT_GAME_SPEED)
-        gameSpeedButton.innerText = GAME_SPEEDS[nextSpeedIndex].name;
-    }
+  if (!PLAY_GAME) {
+    // We can only change the game speed while a game is not going
+    var currentSpeedIndex = GAME_SPEEDS.indexOf(CURRENT_GAME_SPEED);
+    var nextSpeedIndex =
+      currentSpeedIndex < GAME_SPEEDS.length - 1 ? currentSpeedIndex + 1 : 0;
+    CURRENT_GAME_SPEED = GAME_SPEEDS[nextSpeedIndex];
+    console.log("updated game speed:", CURRENT_GAME_SPEED);
+    gameSpeedButton.innerText = GAME_SPEEDS[nextSpeedIndex].name;
+  }
 }
 
 function handleDeath() {
+  // Stop the game loop and reset/update the scores
+  clearInterval(GAME_LOOP);
+  var highScore = Math.max(HIGH_SCORE, gameScore);
+  highScoreText.innerText = "Your High Score: " + highScore;
+  gameScore = 0;
 
-    // Stop the game loop and reset/update the scores
-    clearInterval(GAME_LOOP);
-    var highScore = Math.max(HIGH_SCORE, gameScore);
-    highScoreText.innerText = "Your High Score: " + highScore;
-    gameScore = 0;
+  // Show the death message
+  playButton.classList.remove("fuchsia");
+  playButton.classList.add("red");
+  PAUSE_FOR_DEATH = true;
+  playButton.innerText = "You Have Died";
 
-    // Show the death message
-    playButton.classList.remove('fuchsia');
-    playButton.classList.add('red');
-    PAUSE_FOR_DEATH = true;
-    playButton.innerText = "You Have Died";
+  // Delete the current snake
+  while (snake[0]) {
+    gameGrid.removeChild(snake.pop().el);
+  }
 
-    // Delete the current snake
-    while(snake[0]) {
-        gameGrid.removeChild(snake.pop().el);
-    }
-
-    // Wait for a second, then reinitialize the snake
-    setTimeout(function(){
-        playButton.innerText = "Play Game";
-        playButton.classList.remove('red');
-        playButton.classList.add('fuchsia');
-        // Initialize a new snake
-        snakeHead = new SnakePiece(20, 30);
-        snake.push(new SnakePiece(20, 31));
-        snake.push(new SnakePiece(20, 32));
-        snake.push(new SnakePiece(20, 33));
-        snake.push(new SnakePiece(20, 34));
-        snake.push(snakeHead);
-        PAUSE_FOR_DEATH = false;
-    },1500);
+  // Wait for a second, then reinitialize the snake
+  setTimeout(function () {
+    playButton.innerText = "Play Game";
+    playButton.classList.remove("red");
+    playButton.classList.add("fuchsia");
+    // Initialize a new snake
+    snakeHead = new SnakePiece(20, 30);
+    snake.push(new SnakePiece(20, 31));
+    snake.push(new SnakePiece(20, 32));
+    snake.push(new SnakePiece(20, 33));
+    snake.push(new SnakePiece(20, 34));
+    snake.push(snakeHead);
+    PAUSE_FOR_DEATH = false;
+  }, 1500);
 }
 
 /*
@@ -193,80 +210,84 @@ function handleDeath() {
 
 // Constructor for a new snake piece.
 function SnakePiece(row, col) {
+  var mySnakePieceElement = document.createElement("div");
+  mySnakePieceElement.className = "snake";
+  mySnakePieceElement.style.top = row * BLOCK_SIZE + "px";
+  mySnakePieceElement.style.left = col * BLOCK_SIZE + "px";
+  gameGrid.appendChild(mySnakePieceElement);
 
-    var mySnakePieceElement = document.createElement("div");
-    mySnakePieceElement.className = "snake";
-    mySnakePieceElement.style.top = (row * BLOCK_SIZE) + "px";
-    mySnakePieceElement.style.left = (col * BLOCK_SIZE) + "px";
-    gameGrid.appendChild(mySnakePieceElement);
+  // curRow and curCol are declared inside the function,
+  // so the calling code can't use it. It's javascript's
+  // version of a private variable.
+  //var curRow = row;
+  //var curCol = col;
 
-    // curRow and curCol are declared inside the function,
-    // so the calling code can't use it. It's javascript's
-    // version of a private variable.
-    //var curRow = row;
-    //var curCol = col;
+  this.col = col;
+  this.row = row;
+  this.el = mySnakePieceElement;
 
-    this.col = col;
-    this.row = row;
-    this.el = mySnakePieceElement;
-
-    //this.movePiece = function() {
-    //    this.col += COL_CHANGE[SNAKE_DIRECTION];
-    //    this.row += ROW_CHANGE[SNAKE_DIRECTION];
-    //    this.el.style.top = (this.row * BLOCK_SIZE) + "px";
-    //    this.el.style.left = (this.col * BLOCK_SIZE) + "px";
-    //}
+  //this.movePiece = function() {
+  //    this.col += COL_CHANGE[SNAKE_DIRECTION];
+  //    this.row += ROW_CHANGE[SNAKE_DIRECTION];
+  //    this.el.style.top = (this.row * BLOCK_SIZE) + "px";
+  //    this.el.style.left = (this.col * BLOCK_SIZE) + "px";
+  //}
 }
 
 // Constructor for a new food piece.
 function FoodPiece() {
-    var col = getRandomInt(0, GRID_WIDTH - 1);
-    var row = getRandomInt(0, GRID_HEIGHT - 1);
+  var col = getRandomInt(0, GRID_WIDTH - 1);
+  var row = getRandomInt(0, GRID_HEIGHT - 1);
 
-    var myFoodElement = document.createElement("div");
-    myFoodElement.className = "food";
-    myFoodElement.style.top = (row * BLOCK_SIZE) + "px";
-    myFoodElement.style.left = (col * BLOCK_SIZE) + "px";
-    gameGrid.appendChild(myFoodElement);
+  var myFoodElement = document.createElement("div");
+  myFoodElement.className = "food";
+  myFoodElement.style.top = row * BLOCK_SIZE + "px";
+  myFoodElement.style.left = col * BLOCK_SIZE + "px";
+  gameGrid.appendChild(myFoodElement);
 
-    this.col = col;
-    this.row = row;
-    this.el = myFoodElement;
+  this.col = col;
+  this.row = row;
+  this.el = myFoodElement;
 }
 
 function isDying() {
-    // hits wall
-    var head = snake[0];
-    if (head.row < 0 || head.row >= GRID_HEIGHT || head.col < 0 || head.col >= GRID_WIDTH) {
-        return true;
+  // hits wall
+  var head = snake[0];
+  if (
+    head.row < 0 ||
+    head.row >= GRID_HEIGHT ||
+    head.col < 0 ||
+    head.col >= GRID_WIDTH
+  ) {
+    return true;
+  }
+  // collides with self
+  var i;
+  for (i = 1; i < snake.length; i++) {
+    var curPiece = snake[i];
+    // === is more explicit. It demands that datatypes be the same.
+    // == would return true with '1' == 1, for example.
+    if (head.row === curPiece.row && head.col === curPiece.col) {
+      return true;
     }
-    // collides with self
-    var i;
-    for (i = 1; i < snake.length; i++) {
-        var curPiece = snake[i];
-        // === is more explicit. It demands that datatypes be the same.
-        // == would return true with '1' == 1, for example.
-        if (head.row === curPiece.row && head.col === curPiece.col) {
-            return true;
-        }
-    }
-    return false;
+  }
+  return false;
 }
 
 function isEating() {
-    // eats food
-    var head = snake[0];
-    var i;
-    for (i = 0; i < foods.length; i++) {
-        var curPiece = foods[i];
-        if (head.row === curPiece.row && head.col === curPiece.col) {
-            // Gets rid of food i (since its been eaten)
-            foods.splice(i, 1);
-            gameGrid.removeChild(curPiece.el);
-            return true;
-        }
+  // eats food
+  var head = snake[0];
+  var i;
+  for (i = 0; i < foods.length; i++) {
+    var curPiece = foods[i];
+    if (head.row === curPiece.row && head.col === curPiece.col) {
+      // Gets rid of food i (since its been eaten)
+      foods.splice(i, 1);
+      gameGrid.removeChild(curPiece.el);
+      return true;
     }
-    return false;
+  }
+  return false;
 }
 
 /*
@@ -274,5 +295,5 @@ function isEating() {
 */
 
 function getRandomInt(min, max) {
-    return Math.floor(Math.random() * (max - min + 1)) + min;
+  return Math.floor(Math.random() * (max - min + 1)) + min;
 }
