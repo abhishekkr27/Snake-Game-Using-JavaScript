a1e9fde1eb31b61e35062b96917ba7d1c4a953fd
Abhishek Kumar <abhishekrauniyar2@gmail.com>
2022-04-18 15:47:05 +0530

    fix music

--- js/index.js
+++ js/index.js
@@ -1,9 +1,9 @@
 //     <!-- GAME JAVASCRIPT -->
 
-const foodSound = new Audio("music/food.mp3");
-const gameOverSound = new Audio("music/gameover.mp3");
-const moveSound = new Audio("music/move.mp3");
-const musicSound = new Audio("music/music.mp3");
+const foodSound = new Audio("../music/food.mp3");
+const gameOverSound = new Audio("../music/gameover.mp3");
+const moveSound = new Audio("../music/move.mp3");
+const musicSound = new Audio("../music/music.mp3");
 
 // Environment variables.
 // This code is only executed once.
